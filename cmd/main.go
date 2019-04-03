93128a017a5da07acfeaa04c13713fe78f4903d7
Razvan Lixandru <lixandru@adobe.com>
2019-04-03 17:18:24 +0300

    Added cors

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-    r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
-    })
-    r.Run() // listen and serve on 0.0.0.0:8080
+	r := gin.Default()
+	r.Use(cors.Default())
+	r.GET("/setup", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "setup",
+		})
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run("127.0.0.1:8090") // listen and serve on 0.0.0.0:8080
 }
